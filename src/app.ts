c9976129e7ce89b6870c9cf8861582d8731e2f9b
aliaksandraleanovich <a.lenovich@softteco.com>
2023-06-14 12:06:18 +0300

    feat: add project

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import "reflect-metadata";
+import AuthenticationRoutes from "./controllers/authentication";
+import TestRoutes from "./controllers/test";
+import UserRoutes from "./controllers/users";
+import QuestionsRoutes from "./controllers/questions";
+import AnswersRoutes from "./controllers/answers";
+import express = require("express");
+import bodyParser = require("body-parser");
+import authenticateJWT from "./services/authService";
+
+const app = express();
+const cors = require("cors");
+const { json, urlencoded } = bodyParser;
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+app.use("/", AuthenticationRoutes);
+app.use("/test", authenticateJWT, TestRoutes);
+app.use("/users", authenticateJWT, UserRoutes);
+app.use("/questions", authenticateJWT, QuestionsRoutes);
+app.use("/answers", authenticateJWT, AnswersRoutes);
+
+app.on("error", function (error) {
+    console.error("An error occurred:", error);
+});
+
+export default app;
---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+export { User } from "./User";
+export { Candidate } from "./Candidate";
+export { TestCandidate } from "./TestCandidates";
+export { Test } from "./Test";
+export { Questions } from "./Question";
+export { TestQuestions } from "./TestQuestion";
+export { Answers } from "./Answer";
